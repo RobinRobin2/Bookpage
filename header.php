f5aac558780b13f742c641f1371247187f17d402
“Robin <email@gmail.com>
2023-12-23 21:16:55 +0200

    här kommer header och hero sektionen

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <html>
 <head>
 <title>Boka massage</title> <!-- Titel som syns uppe i "tabben" -->
-<link rel="stylesheet" href="css/style.css"> <!-- Länka in CSS-filen -->
+<link rel="stylesheet" href="css/styles.css"> <!-- Länka in CSS-filen -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400&display=swap" rel="stylesheet">
@@ -18,27 +18,35 @@
 </head>
 <body>
 
-<div id="header">
-  <h1 class="header-text">bookpage</h1>
-</div>
 
-<div id="navigation">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Meny</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Boka massage</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="bookinglist.php">Gjorda bokningar</a>
-        </li>
-      </ul>
-    </div>
+
+<nav class="navbar navbar-expand-lg navbar-light" id="test0">
+  <a class="navbar-brand mx-5" href="#">Bokhandel AB</a>
+  
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end mx-5" id="navbarNav">
+    <ul class="navbar-nav">
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Logga in</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Hem <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item" id="test2">
+        <a class="nav-link" href="#">Böcker</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Om oss</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontakt</a>
+      </li>
+   
+    </ul>
   </div>
 </nav>
 
--- index.php
+++ index.php
@@ -3,11 +3,37 @@
 <?php
 
 	include "header.php"; 
-	?>
+?>
 
 
 
+<body>
+<!-- Hero 1 - Bootstrap Brain Component -->
+	<section class="bg-image d-flex justify-content-center align-items-center"  style="background-image: url('img/bookcover1.jpg'); width: auto; height: 450px;">
+  		<div class="container" >
+    		<div class="row justify-content-md-center">
+      		<div class="col-12 col-md-11 col-lg-9 col-xl-7 col-xxl-6 text-center text-black">
+      		<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+	 		<input id="search-focus" type="search" id="form1" class="form-control" />
+			<button type="button" class="btn btn-light" data-mdb-ripple-init>sök
+	</div>
+</section>	
+
+<section class="my-5">
+<h2 id="popular-categories">Populära kategorier</h2>
+
+<div class="card my-5 " style="width: 15rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</section>
+
+</body>
 
 
 
-<h1>hej</h1>
